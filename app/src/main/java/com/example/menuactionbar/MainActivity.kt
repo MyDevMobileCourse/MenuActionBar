cb43dcf2cf0b80e99a54516c9b14171c7e04380b
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-17 14:36:27 +0100

    add action bar

--- app/src/main/java/com/example/menuactionbar/MainActivity.kt
+++ app/src/main/java/com/example/menuactionbar/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.menuactionbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.action_bar, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.red -> {
+                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.green -> {
+                Toast.makeText(this, "Green", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.blue -> {
+                Toast.makeText(this, "Blue", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
